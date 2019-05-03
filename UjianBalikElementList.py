fd4468938e81e6e099b1580ef239a4ab42f42679
pandupt <50093900+pandupt@users.noreply.github.com>
2019-05-03 14:36:03 +0700

    Add files via upload

---
+++ UjianBalikElementList.py
@@ -0,0 +1,54 @@
+data_list = [1,2,3,4,5,6,7,8,9]
+
+def balikPosisi(data_list):
+    length = len(data_list)
+    s = length
+
+    new_list = [None]*length
+
+    for item in data_list:
+        s = s - 1
+        new_list[s] = item
+    return new_list
+
+
+
+print(balikPosisi(data_list))
+
+###########################################
+
+data_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+def balikPosisi(data_list):
+    length = len(data_list)
+    s = length
+
+    new_list = [None]*length
+
+    for item in data_list:
+        s = s - 1
+        new_list[s] = item
+    return new_list
+
+
+
+print(balikPosisi(data_list))
+
+#############################################
+
+data_list = ['Rakitic', 'Dembele', 'Coutinho', 'Suarez', 'Messi']
+
+def balikPosisi(data_list):
+    length = len(data_list)
+    s = length
+
+    new_list = [None]*length
+
+    for item in data_list:
+        s = s - 1
+        new_list[s] = item
+    return new_list
+
+
+
+print(balikPosisi(data_list))
